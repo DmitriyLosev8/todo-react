2152a6c79d5e750191306e92a8389a7ecba6d16a
DmitriyLosev8 <dmitriylosev8@gmail.com>
2025-10-04 19:36:26 +0300

    add form validation

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -42,23 +42,21 @@ const useTasks = () => {
     )
   },[tasks])
 
-  const addTask = useCallback(() => {
+  const addTask = useCallback((title) => {
 
-    if(newTaskTitle.trim().length > 0) {
-      const newTask ={
-        id: crypto?.randomUUID() ?? Date.now().toString(),
-        title: newTaskTitle,
-        isDone: false,
-      }
-
-      setTasks((prevTasks) => [...prevTasks, newTask])
-      setNewTaskTitle('')
-      setSearchQuery('')
-      newTaskInputRef.current.focus()
+    const newTask ={
+      id: crypto?.randomUUID() ?? Date.now().toString(),
+      title,
+      isDone: false,
     }
 
+    setTasks((prevTasks) => [...prevTasks, newTask])
+    setNewTaskTitle('')
+    setSearchQuery('')
     newTaskInputRef.current.focus()
-  },[newTaskTitle])
+
+    newTaskInputRef.current.focus()
+  },[])
 
   useEffect(() => {
     saveTasks(tasks)
